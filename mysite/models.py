6821e9d7d29c0e39f742d9c41e816413adae4a74
hsson95 <gudtjs0428@gmail.com>
2022-10-19 22:31:19 +0900

    db_update

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Login(models.Model):
+    # username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    username = models.CharField(max_length=200)
+    number = models.IntegerField()
+
+
